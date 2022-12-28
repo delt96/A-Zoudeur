db89d652cabb83b277471471373efd6be868cdfc
범수 <delt96@naver.com>
2022-12-28 23:20:36 +0900

    2022-12-28-com

---
+++ js/A-Zodeur subscribe.js
@@ -0,0 +1,64 @@
+// function Explanation(){
+//   console.log("변경")
+//   $(".Explanation").css("display", "block")
+// }
+// function ExplanationOff(){
+//   console.log("변경")
+//   $(".Explanation").css("display", "none")
+// }
+
+$(document).ready(function(){
+  $(".help").hover(function(){
+    $(".Explanation").css("display","block")
+  },function(){
+    $(".Explanation").css("display", "none")
+  })
+  
+  $(".citrus").hover(function(){
+    $(".citrusText").css("display","block")
+  },function(){
+    $(".citrusText").css("display", "none")
+  })
+
+  $(".green").hover(function(){
+    $(".greenText").css("display","block")
+  },function(){
+    $(".greenText").css("display", "none")
+  })
+
+  $(".whiteMusk").hover(function(){
+    $(".whiteMuskText").css("display","block")
+  },function(){
+    $(".whiteMuskText").css("display", "none")
+  })
+
+  $(".vanilla").hover(function(){
+    $(".vanillaText").css("display","block")
+  },function(){
+    $(".vanillaText").css("display", "none")
+  })
+
+  $(".herb").hover(function(){
+    $(".herbText").css("display","block")
+  },function(){
+    $(".herbText").css("display", "none")
+  })
+  
+  $(".woody").hover(function(){
+    $(".woodyText").css("display","block")
+  },function(){
+    $(".woodyText").css("display", "none")
+  })
+
+  $(".floral").hover(function(){
+    $(".floralText").css("display","block")
+  },function(){
+    $(".floralText").css("display", "none")
+  })
+
+  $(".selectBox img, radio").on("click",function(e){
+    const alt= e.target.alt
+    $(`#${alt}`).attr("checked", true);
+
+  })
+})
